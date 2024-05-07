ebf00ec80679f19b888f41d8f5b35e60afff841c
amailemon98 <amailemon98@gmail.com>
2024-05-07 21:57:55 +0900

    Restful API, Get, Post, Delete사용

--- index.js
+++ index.js
@@ -26,149 +26,195 @@ server.on("request",async (req, res)=>{
         case '.js':
             contentType = 'text/javascript'; break;
     }
-    console.log(contentType);
+    // console.log(contentType);
 
-    if(req.url === '/login'){
-        // const myPath = path.join(__dirname, req.url.slice(1), req.url.slice(1)+'.html');
-        const myPath = path.join(__dirname, '/views', 'login.html'); //path.join이 알아서 /,\ 가지런하게 정렬해준다.
-        // C://shopping_mall/views/login.html
+    if(req.method === 'GET'){
 
-        const data = await fs.readFile(myPath, 'utf-8');
-        res.setHeader('Content-Type', contentType);
-        res.write(data);
-        res.end();
-    }
-
-    if(req.url === '/login' && req.method === 'POST' ){
-
-        console.log('/login post');
-
-        // chunk : 메모리로 읽어들임
-        let body = '';//따로 저장하기 위한 공간
-
-        req.on('data', (chunk)=>{
-            body += chunk.toString(); // 순수 node의 방법
-        })
-
-        // body : JSON.stringify({name : input.value})
-        req.on('end', async ()=>{
-            const {name} = JSON.parse(body);
-            console.log(name);
-
-            const user_json = await fs.readFile(path.join(__dirname,'/data', 'user.json'), "utf-8")
-            console.log(user_json); // {"name": "철수"} 문자열 형태
-            const user_data = JSON.parse(user_json); // 자바스크립트 파일로 변환
-            console.log(user_data); // js 상태
-
-            user_data.push({name}) // === name : name // append
-
-            if(!fsExit.existsSync('./data')){ //없을때 만든다
-                fsExit.mkdirSync('./data');//현재 폴더 안에 data 만들기
-            }
-            fs.writeFile(path.join(__dirname,'/data', 'user.json'),// 파일은 없으면 만든다.
-            JSON.stringify(user_data, null, "  "),
-            err => {if(err) console.log(err)}
-            )
-        });
-
-    }
-
-    if(req.url === '/ha_swap'){
-        // const myPath = path.join(__dirname, req.url.slice(1), req.url.slice(1)+'.html');
-        const myPath = path.join(__dirname, 'ha_swap', 'ha_swap.html'); //path.join이 알아서 /,\ 가지런하게 정렬해준다.
-        // C://shopping_mall/ha_swap/ha_swap.html
-
-        const data = await fs.readFile(myPath, 'utf-8');
-        res.setHeader('Content-Type', contentType);
-        res.write(data);
-        res.end();
-    }
-
-    if(req.url.includes('ha_swap.css')){ //확장자가 css라면
-        const myPath = path.join(__dirname, 'ha_swap', 'ha_swap.css'); //css 불러오기
-        const data = await fs.readFile(myPath, 'utf-8');
-        // res.setHeader('Content-Type', contentType);
-        res.setHeader('Content-Type', 'text/css');
-        res.write(data);
-        res.end();
-    }
-
-    if(req.url === '/'){    
-        const data = await fs.readFile('./views/index.html', 'utf-8');
-
-        res.setHeader('Content-Type', contentType);
-        res.write(data);
-        res.end();
-    }
-
-    // /styles/reset.css
-    if(req.url.includes('/reset.css')){    
-        const data = await fs.readFile('./styles/reset.css', 'utf-8');
-
-        res.setHeader('Content-Type', contentType);
-        res.write(data);
-        res.end();
-    }
-
-    if(req.url.includes('.png')||req.url.includes('.jpg')||req.url.includes('.gif')){   
-        const data = await fs.readFile(path.join(__dirname, req.url));
-
-        // res.setHeader('Content-Type', contentType);
-        const extName = path.extname(req.url)
-        res.setHeader('Content-Type', `image/${extName}`);
-        res.write(data);
-        res.end();
-    }
-
-    // if(req.url.includes('.png')||req.url.includes('.jpg')||req.url.includes('.gif')){
+        // get : localhost:4500
+        if(req.url === '/'){    
+            const data = await fs.readFile('./views/index.html', 'utf-8');
+    
+            res.setHeader('Content-Type', contentType);
+            res.write(data);
+            res.end();
         
-    //     const myPath = path.join(__dirname, req.url);
-    //     // /images/images.png
+        // get : localhost:4500/user_delete
+        }else if(req.url === '/user_delete'){
+            const myPath = path.join(__dirname, '/views', 'delete.html'); 
+    
+            const data = await fs.readFile(myPath, 'utf-8');
+            res.setHeader('Content-Type', contentType);
+            res.write(data);
+            res.end();
 
-    //     // const extName = path.extname(myPath); //확장자 뽑아내기
-    //     console.log('path : ',myPath);
-    //     console.log('extName : ',extName);
-    //     // path :  C:\shopping_mall\images\images.png
-    //     // extName :  .png
+        // get : localhost:4500/login
+        }else if(req.url === '/login'){
+            const myPath = path.join(__dirname, '/views', 'login.html'); 
+    
+            const data = await fs.readFile(myPath, 'utf-8');
+            res.setHeader('Content-Type', contentType);
+            res.write(data);
+            res.end();
 
-    //     const data = await fs.readFile(myPath);
+        // get : localhost:4500/ha_swap
+        }else if(req.url === '/ha_swap'){
+            const myPath = path.join(__dirname, 'ha_swap', 'ha_swap.html');
+    
+            const data = await fs.readFile(myPath, 'utf-8');
+            res.setHeader('Content-Type', contentType);
+            res.write(data);
+            res.end();
+        // get : localhost:4500/ha_swap.css
+        }else if(req.url.includes('ha_swap.css')){ 
+            const myPath = path.join(__dirname, 'ha_swap', 'ha_swap.css');
+            const data = await fs.readFile(myPath, 'utf-8');
+            res.setHeader('Content-Type', 'text/css');
+            res.write(data);
+            res.end();
+        // get : localhost:4500/product 
+        }else if(req.url === '/product'){    
+            const data = await fs.readFile('./views/product.html', 'utf-8');
+    
+            res.setHeader('Content-Type', contentType);
+            res.write(data);
+            res.end();
+        // get : localhost:4500/about
+        }else if(req.url === '/about'){    
+            const data = await fs.readFile('./views/about.html', 'utf-8');
+    
+            res.setHeader('Content-Type', contentType);
+            res.write(data);
+            res.end();
+        // get : localhost:4500/contact
+        }else if(req.url === '/contact'){    
+            const data = await fs.readFile('./views/contact.html', 'utf-8');
+    
+            res.setHeader('Content-Type', contentType);
+            res.write(data);
+            res.end();
+        // get : localhost:4500/reset.css  문서가 요청
+        }else if(req.url.includes('/reset.css')){    
+            const data = await fs.readFile('./styles/reset.css', 'utf-8');
+    
+            res.setHeader('Content-Type', contentType);
+            res.write(data);
+            res.end();
+        // get : localhost:4500/ 각종 이미지 확장자 요청 : 문서가 요청
+        }else if(req.url.includes('.png')||req.url.includes('.jpg')||req.url.includes('.gif')){   
+            const data = await fs.readFile(path.join(__dirname, req.url));
+    
+            // res.setHeader('Content-Type', contentType);
+            const extName = path.extname(req.url)
+            res.setHeader('Content-Type', `image/${extName}`);
+            res.write(data);
+            res.end();
+        // get : localhost:4500/index.js : 문서가 요청
+        }else if(req.url.includes('/index.js')){    
+            const data = await fs.readFile('./scripts/index.js');
+    
+            res.setHeader('Content-Type', 'text/javascript');
+            res.write(data);
+            res.end();
+        }
 
-    //     // const extName = path.extname(req.url);
-    //     // const data = await fs.readFile("./"+req.url);
-    //     res.setHeader('Content-Type', `image/${extName}`);
-    //     res.write(data);
-    //     res.end();
-    // }
+    // get method end
+    }else if(req.method === 'POST'){
+        // post : localhost:4500/login
+        if(req.url === '/login' && req.method === 'POST' ){
 
-    if(req.url.includes('/index.js')){    
-        const data = await fs.readFile('./scripts/index.js');
+            console.log('/login post');
+    
+            // chunk : 메모리로 읽어들임
+            let body = '';//따로 저장하기 위한 공간
+    
+            req.on('data', (chunk)=>{
+                body += chunk.toString(); // 순수 node의 방법
+            })
+    
+            // body : JSON.stringify({name : input.value})
+            req.on('end', async ()=>{
+                const {name, pwd} = JSON.parse(body);
+                console.log(name, pwd); // 터미널 확인
+    
+                const user_json = await fs.readFile(path.join(__dirname,'/data', 'user.json'), "utf-8")
+                console.log(user_json); // {"name": "철수"} 문자열 형태
+                const user_data = JSON.parse(user_json); // 자바스크립트 파일로 변환
+                console.log(user_data); // js 상태
+    
+                user_data.push({name, pwd}) // === name : name // append
+    
+                if(!fsExit.existsSync('./data')){ //없을때 만든다
+                    fsExit.mkdirSync('./data');//현재 폴더 안에 data 만들기
+                }
+                fs.writeFile(path.join(__dirname,'/data', 'user.json'),// 파일은 없으면 만든다.
+                JSON.stringify(user_data, null, "  "),
+                err => {if(err) console.log(err)});
+                
+                // 이상없음을 응답해준다.
+                // res.statusCode = 200;
+                res.writeHead(201, {'Content-type': 'text/html'})
+                res.write(JSON.stringify({success : "성공", message : "회원가입 정상등록"}));
+                // 어떤 요건으로 응답할 것인가? 에 대한 규칙
+                res.end();
+            });
+        }else if(req.url === '/sign-up'){
 
-        res.setHeader('Content-Type', 'text/javascript');
-        res.write(data);
-        res.end();
+        }else if(req.url === '/product'){
+
+        }
+
+    // post method end
+
+    // params : 꺼내는 메서드, 파라미터로 데이터를 넘긴다는 의미
+    // localhost:4500/user/name=지울이름
+    // localhost:4500/user/지울이름
+    // localhost:4500/user/1(id)
+    // localhost:4500/user/2(id)
+    // localhost:4500/user/3(id)
+    }else if(req.method === 'DELETE'){
+        console.log('delete user');
+        // localhost:4500/user/hello
+        if(req.url.includes('user')){
+
+            const url = req.url;
+            console.log(url); // /user/hello
+
+            const slash = url.split('/');
+            console.log(slash); // [ '', 'user', 'hello' ]   slash[2] === hello
+
+            const datas = await fs.readFile('./data/user.json', 'utf-8');
+            const users = await JSON.parse(datas); // js
+
+            // const index = users.map((data, index)=>data.name); 
+            // const index = users.find((data, index)=> data.id === +slash[2]); //find는 데이터 하나만 찾기 // +로 문자를 숫자로 변환하여 비교
+            const user_data = users.filter((data, index)=> data.id !== +slash[2]); //filter는 여러개 데이터 찾기
+
+            // console.log('index : ', index); // index :  [ '영희', '철수', '길동' ]
+            // console.log('index : ', user_data); // index :  [ '영희', '철수', '길동' ]
+
+            try{
+                fs.writeFile(path.join(__dirname,'/data', 'user.json'),// 파일은 없으면 만든다.
+                JSON.stringify(user_data, null, "  "),
+                err => {if(err) throw err});
+
+                res.writeHead(201, {'Content-type': 'text/html'})
+                res.write(JSON.stringify({success : "성공", message : "삭제완료"}));
+                res.end();
+            }catch(err){
+                res.writeHead(503, {'Content-type': 'text/html'})
+                res.write(JSON.stringify({success : "실패", message : "삭제불가"}));
+                res.end();
+            }
+        }
+
+    // delete method end
+    }else if(req.method === 'PUT'){
+
+    // put method end
+    }else{
+        res.statusCode = 503;
     }
-
-    if(req.url === '/product'){    
-        const data = await fs.readFile('./views/product.html', 'utf-8');
-
-        res.setHeader('Content-Type', contentType);
-        res.write(data);
-        res.end();
-    }
-    if(req.url === '/about'){    
-        const data = await fs.readFile('./views/about.html', 'utf-8');
-
-        res.setHeader('Content-Type', contentType);
-        res.write(data);
-        res.end();
-    }
-    if(req.url === '/contact'){    
-        const data = await fs.readFile('./views/contact.html', 'utf-8');
-
-        res.setHeader('Content-Type', contentType);
-        res.write(data);
-        res.end();
-    }
+    
 })
 
 server.listen(PORT,()=>{
